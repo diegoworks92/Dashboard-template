e2f13298004aaf0904c73e0d536d43937b7a2bc2
diegoworks92 <diegoreyes@diegoworks.com>
2024-01-18 18:01:41 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,156 @@
+import {
+  RiArrowDownSLine,
+  RiNotification3Line,
+  RiSettings3Line,
+  RiLogoutCircleRLine,
+  RiThumbUpLine,
+  RiChat3Line,
+} from "react-icons/ri";
+import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/slide.css";
+import { Link } from "react-router-dom";
+const Header = () => {
+  const date = new Date().toLocaleDateString();
+  return (
+    <header className="h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8 flex items-center justify-end">
+      <nav className="flex items-center gap-2">
+        <Menu
+          menuButton={
+            <MenuButton className="relative hover:bg-secondary-100 p-2 rounded-lg transition-colors">
+              {" "}
+              <RiNotification3Line />
+              <span className="absolute -top-0.5 -right-0 bg-primary py-0.5 px-[5px] box-content text-black rounded-full text-[8px] font-bold">
+                3
+              </span>
+            </MenuButton>
+          }
+          transition
+          align="center"
+          arrow
+          menuClassName="bg-secondary-100 p-4"
+        >
+          <h1 className="text-gray-300 text-center font-medium">
+            Notifications (3)
+          </h1>
+          <hr className="my-6 border-gray-500" />
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/"
+              className="text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
+            >
+              <img
+                src="https://img.freepik.com/foto-gratis/negocio-joven-empresario-profesional-gafas-sonriendo-camara-brazo-cruzado-sobre-pecho_1258-26377.jpg"
+                className="w-8 h-8 object-cover rounded-full"
+              />
+              <div className="text-sm flex flex-col">
+                <div className="flex items-center justify-between gap-4">
+                  <span>Diego Reyes</span>
+                  <span className="text-[8px]">{date}</span>
+                </div>
+                <p className="text-gray-500 text-xs">Lorem ipsum dolor...</p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/"
+              className="text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
+            >
+              <RiThumbUpLine className="p-2 bg-blue-200 text-blue-700 box-content rounded-full" />
+              <div className="text-sm flex flex-col">
+                <div className="flex items-center justify-between gap-4">
+                  <span>New like</span>
+                  <span className="text-[8px]">{date}</span>
+                </div>
+                <p className="text-gray-500 text-xs">
+                  Diego likes your post...
+                </p>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/"
+              className="text-gray-300 flex flex-1 items-center gap-4 py-2 px-4 hover:bg-secondary-900 transition-colors rounded-lg"
+            >
+              <RiChat3Line className="p-2 bg-yellow-200 text-yellow-700 box-content rounded-full" />
+              <div className="text-sm flex flex-col">
+                <div className="flex items-center justify-between gap-4">
+                  <span>New commente</span>
+                  <span className="text-[8px]">{date}</span>
+                </div>
+                <p className="text-gray-500 text-xs">Diego commented on...</p>
+              </div>
+            </Link>
+          </MenuItem>
+          <hr className="my-6 border-gray-500" />
+          <MenuItem className="p-0 hover:bg-transparent flex justify-center cursor-default">
+            <Link
+              to="/"
+              className="text-gray-400 text-sm hover:text-white transition-colors"
+            >
+              All notifications
+            </Link>
+          </MenuItem>
+        </Menu>
+        <Menu
+          menuButton={
+            <MenuButton className="flex items-center gap-x-2 hover:bg-secondary-100 p-2 rounded-lg transition-colors">
+              {" "}
+              <img
+                src="https://img.freepik.com/foto-gratis/negocio-joven-empresario-profesional-gafas-sonriendo-camara-brazo-cruzado-sobre-pecho_1258-26377.jpg"
+                className="w-6 h-6 object-cover rounded-full"
+              />
+              <span>Diego Reyes</span>
+              <RiArrowDownSLine />
+            </MenuButton>
+          }
+          align="end"
+          arrow
+          /*           arrowClassName="bg-secondary-100" */
+          transition
+          menuClassName="bg-secondary-100 p-4"
+        >
+          {" "}
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/perfil"
+              className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+            >
+              <img
+                src="https://img.freepik.com/foto-gratis/negocio-joven-empresario-profesional-gafas-sonriendo-camara-brazo-cruzado-sobre-pecho_1258-26377.jpg"
+                className="w-8 h-8 object-cover rounded-full"
+              />
+              <div className="flex flex-col text-sm">
+                <span className="text-sm">Diego Reyes</span>
+                <span className="text-xs text-gray-500">
+                  diegoreyes@diegoworks.com
+                </span>
+              </div>
+            </Link>
+          </MenuItem>
+          <hr className="my-4 border-gray-500" />
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/setting"
+              className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+            >
+              <RiSettings3Line /> Setting
+            </Link>
+          </MenuItem>
+          <MenuItem className="p-0 hover:bg-transparent">
+            <Link
+              to="/log-out"
+              className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+            >
+              <RiLogoutCircleRLine /> Log out
+            </Link>
+          </MenuItem>
+        </Menu>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,137 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  RiBarChart2Line,
+  RiEarthLine,
+  RiMessage3Line,
+  RiCalendarTodoLine,
+  RiLogoutCircleRLine,
+  RiArrowRightSLine,
+  RiMenu3Line,
+  RiCloseLine,
+  RiCustomerService2Line,
+} from "react-icons/ri";
+const Sidebar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showSubmenu, setShowSubmenu] = useState(false);
+
+  return (
+    <>
+      <div
+        className={`xl:h-[100vh] overflow-y-scroll fixed xl:static  w-[80%] md:w-[40%] lg:w-[30%] xl:w-auto h-full top-0 bg-secondary-100 p-4 flex flex-col justify-between z-50 ${
+          showMenu ? "left-0" : "-left-full"
+        } transition-all`}
+      >
+        <div>
+          <h1 className="text-center text-2xl font-bold text-white mb-10">
+            Admin <span className="text-primary text-4xl">.</span>
+          </h1>
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+              >
+                <RiBarChart2Line className="text-primary" /> Analytic
+              </Link>
+            </li>
+            <li>
+              <button
+                onClick={() => setShowSubmenu(!showSubmenu)}
+                className="w-full flex items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+              >
+                <span className="flex items-center gap-4">
+                  <RiEarthLine className="text-primary" /> Social media
+                </span>
+                <RiArrowRightSLine
+                  className={`mt-1 ${
+                    showSubmenu && "rotate-90"
+                  } transition-all`}
+                />
+              </button>
+              {/*               <ul className={`my-2 ${!showSubmenu && "hidden"}`}> */}
+              <ul
+                className={`${
+                  showSubmenu ? "h-[130px]" : "h-0"
+                } overflow-hidden transition-all`}
+              >
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-primary before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white"
+                  >
+                    Social Media Post
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white"
+                  >
+                    Social Media Statistic
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white"
+                  >
+                    Social Media Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className="py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3 before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px] before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 hover:text-white"
+                  >
+                    Social Media Followers
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+              >
+                <RiMessage3Line className="text-primary" /> Message
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+              >
+                <RiCustomerService2Line className="text-primary" /> Support
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+              >
+                <RiCalendarTodoLine className="text-primary" /> Schedule
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <nav>
+          <Link
+            to="/"
+            className="flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors"
+          >
+            <RiLogoutCircleRLine className="text-primary" /> Log Out
+          </Link>
+        </nav>
+      </div>
+      <button
+        onClick={() => setShowMenu(!showMenu)}
+        className="xl:hidden fixed bottom-4 right-4 bg-primary text-black p-3 rounded-full z-50"
+      >
+        {showMenu ? <RiCloseLine /> : <RiMenu3Line />}
+      </button>
+    </>
+  );
+};
+
+export default Sidebar;
